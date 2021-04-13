9f43383dbe921e3c1de33f51bdffc6bdce22187f
Cameron Imanpour <34729011+CIMAN01@users.noreply.github.com>
2021-04-13 15:18:33 -0700

    Add Level 3 Authentication/Security
    Add Level 3 Security to the Authentication process by Hashing Passwords

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ const { dirname } = require("path"); // path
 const _ = require("lodash"); // lodash
 const ejs = require("ejs"); // ejs
 const mongoose = require("mongoose"); // mongoose
-const encrypt = require("mongoose-encryption"); // mongoose-encryption
+//const encrypt = require("mongoose-encryption"); // mongoose encryption
+const md5 = require("md5"); // hash function (a weak hashing algorithm)
 const express = require("express"); // express
 
 // create a new application that uses express
@@ -41,13 +42,12 @@ const userSchema = new mongoose.Schema ({
     password: String
 }); 
 
-// create a secret string (encryption key)
-////const secret = "Thisisalongsecretstring."; // unsafe way of storing an encryption key
-// const secret has instead been added to hidden .env file (environment specific variable) 
+
+// refer to environment specific variable (SECRET) inside .env file for they encryption key
 
 // add encrypt package as a plugin to the Schema (must be created prior to Model creation)
-//userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"] }); // no longer needed since we use .env
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"] }); // enable encryption for the password field only
+// use process.env.* to access the KEY inside the .env file and use encryptedFields to encrypt only the password field
+//userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"] }); 
 
 
 // create users (which will be added to userDB) via the mongoose Model based on userSchema
@@ -55,17 +55,17 @@ const User = mongoose.model("User", userSchema); // "User" becomes "users" behin
 
 
 // GET request for the home route
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
     res.render("home");
 });
 
 // GET request for the login route
-app.get("/login", function(req, res){
+app.get("/login", function(req, res) {
     res.render("login");
 });
 
 // GET request for the register route
-app.get("/register", function(req, res){
+app.get("/register", function(req, res) {
     res.render("register");
 });
 
@@ -74,7 +74,7 @@ app.post("/register", function(req, res){
     // create a mongoose Document for each new user based on the Model 
     const newUser = new User({ // create a new user 
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password) // convert password to an md5 hash 
     });
     // *** during save, documents are encrypted and then signed by Mongoose ***
     // save the document to database (save new user) 
@@ -92,7 +92,7 @@ app.post("/register", function(req, res){
 app.post("/login", function(req, res) {
     // grab and store (body-parse) username/password fields from the form
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password); // hash conversion of password
     // *** during find, documents are authenticated and then decrypted by Mongoose ***
     // find a match between the fields entered and the ones already stores in the database
     User.findOne({email: username}, function(err, foundUser) { // add a callback to handle any errors
@@ -104,10 +104,10 @@ app.post("/login", function(req, res) {
         else {
             // if a user is found in the database
             if(founderUser) {
-                // if the user password matches the password found in the database
+                // if the hashed user password matches the hashed password found in the database
                 if(foundUser.password === password) {
                     // authentication is successful 
-                    res.render("secrets"); //  render the secrets page 
+                    res.render("secrets"); // render the secrets page 
                 }
             }  
         }
