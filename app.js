d71f736f383584aae19bee95894b025112cc3a4b
Cameron Imanpour <34729011+CIMAN01@users.noreply.github.com>
2021-04-15 13:42:08 -0700

    Use Passport to Add Cookies and Sessions (Level 5)
    Add level 5 security/authentication by using Passport.js to implement cookies that the browser stores which hold the user login credentials for that session. This allows the website to 'remember' when a user is already logged in so that when they browse around, or access a part of the website that requires authentication, they don't need to keep logging in.

--- app.js
+++ app.js
@@ -13,13 +13,18 @@ const port = 3000;
 const https = require("https"); // https
 const { dirname } = require("path"); // path
 const _ = require("lodash"); // lodash
+const express = require("express"); // express
 const ejs = require("ejs"); // ejs
 const mongoose = require("mongoose"); // mongoose
 //const encrypt = require("mongoose-encryption"); // mongoose encryption
 //const md5 = require("md5"); // hash function (a weak hashing algorithm)
-const bcrypt = require("bcrypt"); // bcrypt hash function 
-const saltRounds = 10; // salt rounds
-const express = require("express"); // express
+//const bcrypt = require("bcrypt"); // bcrypt hash function 
+//const saltRounds = 10; // salt rounds
+const session = require("express-session"); // session
+const passport = require("passport"); // Passport
+//const passportLocal = require("passport-local"); // not needed (redundant)
+const passportLocalMongoose = require("passport-local-mongoose"); 
+
 
 // create a new application that uses express
 const app = express();
@@ -32,11 +37,24 @@ app.use(express.urlencoded({extended: true}));
 // use public folder to store static files (such as images and css code) 
 app.use(express.static("public")); 
 
+// set up and use express-session
+app.set('trust proxy', 1) // trust first proxy 
+app.use(session({
+    secret: 'A secret session',
+    resave: false, // true recommended by documentation 
+    saveUninitialized: false, // // true recommended by documentation 
+    cookie: { secure: true }
+  }));
+
+// initialize and use passport
+app.use(passport.initialize());
+app.use(passport.session()); // use passport to manage session 
 
 // allow mongoose to connect to a local mongoDB instance (userDB)
 mongoose.connect("mongodb://localhost:27017/userDB",  
                     {useNewUrlParser: true, useUnifiedTopology: true}); 
-
+// needed to get rid of deprecation warning
+mongoose.set("useCreateIndex", true);
 
 // create a new object from the mongoose Schema class that holds the email and password fields
 const userSchema = new mongoose.Schema ({
@@ -44,17 +62,17 @@ const userSchema = new mongoose.Schema ({
     password: String
 }); 
 
-
-// refer to environment specific variable (SECRET) inside .env file for they encryption key
-
-// add encrypt package as a plugin to the Schema (must be created prior to Model creation)
-// use process.env.* to access the KEY inside the .env file and use encryptedFields to encrypt only the password field
-//userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"] }); 
-
+// use plugin for hashing/salting passwords and storing them in the mongoose db
+userSchema.plugin(passportLocalMongoose);
 
 // create users (which will be added to userDB) via the mongoose Model based on userSchema
 const User = mongoose.model("User", userSchema); // "User" becomes "users" behind the scenes
 
+// use "createStrategy" INSTEAD OF "authenticate" (simplified config)
+passport.use(User.createStrategy()); // create a local login strategy
+// use static serialize and deserialize of model for passport session support
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // GET request for the home route
 app.get("/", function(req, res) {
@@ -71,6 +89,27 @@ app.get("/register", function(req, res) {
     res.render("register");
 });
 
+// GET request for the secrets route
+app.get("/secrets", function(req, res) {
+    // if user is authenticated
+    if (req.isAuthenticated()) {
+        // reveal secrets page
+        res.render("secrets");
+    }
+    // if user is not authenticated
+    else {
+        // make the user login first before accessing secrets page
+        res.redirect("/login"); // redirect to login route
+    }
+});
+  
+// GET request for the logout route
+app.get("/logout", function(req, res) {
+    req.logout(); // de-authenticate user and end user session by logging out 
+    res.redirect("/"); // go back to home route
+});
+
+/*
 // POST request for the register route (triggers when user clicks submit in the form)
 app.post("/register", function(req, res) {
     // use bcrypt as a hashing function and include rounds of salting 
@@ -96,8 +135,31 @@ app.post("/register", function(req, res) {
         });
     });
 });
+*/
+
+// POST request for the register route (triggers when user clicks submit in the form)
+app.post("/register", function(req, res) {
+    // use Passport-Local Mongoose to register users and add a callback function to handle errors
+    User.register({username: req.body.username}, req.body.password, function(err, user) {
+        // if there are any errors
+        if (err) {
+            console.log(err); // log errors 
+            // redirect to the register route so the user can try again
+            res.redirect("/register"); 
+        }
+        // if there aren't any errors
+        else {
+            // use passport to authenticate user (authenticaton type is local)
+            passport.authenticate("local")(req, res, function() { 
+// callback is only triggered if authentication is successful (manage to set up a cookie that saves the user login session)
+                res.redirect("/secrets"); // redirect to the secrets route 
+            });
+        }
+    });    
+});
 
 
+/*
 // POST request for the login route
 app.post("/login", function(req, res) {
     // grab and store (body-parse) username/password fields from the form
@@ -127,11 +189,35 @@ app.post("/login", function(req, res) {
         }
     });
 });
+*/
 
-// 
+// POST request for the login route
+app.post("/login", function(req, res) {
+    // create a new user from the mongoose model with a username and password
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+      // use passport's login() function to establish a login session 
+      req.login(user, function(err) { // use callback function to handle errors
+        // if there are any errors
+        if (err) {
+            // log errors
+            console.log(err);
+        }
+        // if there aren't any errors
+        else {
+            // use passport to authenticate the user via a local strategy 
+            passport.authenticate("local")(req, res, function() {
+                // callback is only triggered if authentication is successful
+                res.redirect("/secrets"); // redirect to the secrets route 
+            });
+        }
+    });
+});   
+
 
 // port listening 
 app.listen(port, function() {
     console.log("Server started on port 3000");
-  });
-  
+});
